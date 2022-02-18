cd0ed51c87a232879f7425caff71ede72f771082
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2022-02-19 00:07:45 +0900

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,12 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public')
+    },
+    compress: true,
+    port: 9000
   }
 }
