3bcad76bbd57159a859ca3d9147a663d5db2acdb
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2022-02-21 19:21:42 +0900

    Update Parser to use prefixParseFunctions

--- src/parser.ts
+++ src/parser.ts
@@ -1,11 +1,41 @@
 import { Expression, IntegerLiteral } from './ast'
 import { Lexer } from './lexer'
+import { Token } from './token'
+
+interface ParseFunctions {
+  [key: string]: () => Expression
+}
 
 export class Parser {
-  constructor(private lexer: Lexer) {}
+  private currentToken: Token
+
+  private prefixParseFunctions: ParseFunctions = {}
+  // private infixParseFunctions: ParseFunctions = {}
+
+  constructor(private lexer: Lexer) {
+    this.prefixParseFunctions['INTEGER'] = this.parseIntegerLiteral.bind(this)
+
+    this.currentToken = this.lexer.nextToken()
+  }
 
   parseExpression(): Expression {
-    const token = this.lexer.nextToken()
+    const prefixParseFunction =
+      this.prefixParseFunctions[this.currentToken.type]
+    if (!prefixParseFunction) {
+      throw new Error(
+        `prefixParseFunction not exists. this.currentToken.type = ${this.currentToken.type}`
+      )
+    }
+    const leftExpression = prefixParseFunction()
+    return leftExpression
+  }
+
+  // prefixParseFunctions
+
+  private parseIntegerLiteral(): IntegerLiteral {
+    const token = this.currentToken
     return new IntegerLiteral(token, Number(token.literal))
   }
+
+  // infixParseFunctions
 }
